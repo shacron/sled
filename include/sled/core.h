c0154541f300c4c35015ad4aba1d39b2fe49eeff
Shac Ron <shac.ron@gmail.com>
2023-02-01 15:41:32 -0800

    move RV_REGs to public header

--- include/sled/core.h
+++ include/sled/core.h
@@ -1,26 +1,5 @@
-// MIT License
-
-// Copyright (c) 2022 Shac Ron
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 // SPDX-License-Identifier: MIT License
+// Copyright (c) 2022-2023 Shac Ron and The Sled Project
 
 #pragma once
 
@@ -56,6 +35,14 @@ struct core_params {
 #define CORE_REG_LR     0xfffd
 #define CORE_REG_ARG0   0xfffc
 #define CORE_REG_ARG1   0xfffb
+
+// Architecture-specific registers definitions
+#define RV_REG_MSCRATCH 0x80000000
+#define RV_REG_MEPC     0x80000001
+#define RV_REG_MCAUSE   0x80000002
+#define RV_REG_MTVAL    0x80000003
+#define RV_REG_MIP      0x80000004
+
 
 void core_set_reg(core_t *c, uint32_t reg, uint64_t value);
 uint64_t core_get_reg(core_t *c, uint32_t reg);
