0a85bd5c0544662f467204ee635a0337d734d9ec
Shac Ron <shac.ron@gmail.com>
2024-01-02 18:44:17 -0800

    Fix list prototypes

--- include/sled/list.h
+++ include/sled/list.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT License
-// Copyright (c) 2023 Shac Ron and The Sled Project
+// Copyright (c) 2023-2024 Shac Ron and The Sled Project
 
 #pragma once
 
@@ -36,14 +36,14 @@ static inline bool sl_list_is_empty(sl_list_t *list) { return list->first == NUL
 void sl_list_remove_node(sl_list_t *list, sl_list_node_t *n, sl_list_node_t *prev);
 
 // finding requires traversing the list
-int list_find_and_remove(sl_list_t *list, sl_list_node_t *n);
+int sl_list_find_and_remove(sl_list_t *list, sl_list_node_t *n);
 
 void sl_list_insert_sorted(sl_list_t *list, int(*compare)(const sl_list_node_t *, const sl_list_node_t *), sl_list_node_t *n);
 
-void list_interator_begin(sl_list_iterator_t *iter, sl_list_t *list);
-sl_list_node_t * list_interator_next(sl_list_iterator_t *iter);
-sl_list_node_t * list_iterator_get_current(sl_list_iterator_t *iter);
-void list_iterator_remove_current(sl_list_iterator_t *iter);
+void sl_list_interator_begin(sl_list_iterator_t *iter, sl_list_t *list);
+sl_list_node_t * sl_list_interator_next(sl_list_iterator_t *iter);
+sl_list_node_t * sl_list_iterator_get_current(sl_list_iterator_t *iter);
+void sl_list_iterator_remove_current(sl_list_iterator_t *iter);
 
 #ifdef __cplusplus
 }
